eecf6195a502965a0adc4bc6e55fd19cd151a9b5
mnandamello <mfernanda.nintendo@gmail.com>
2024-05-11 11:19:25 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CineWave.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
