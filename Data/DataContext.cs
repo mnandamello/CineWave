8f77b7819b65abd1cab286a7714c3e3273bddb46
mnandamello <mfernanda.nintendo@gmail.com>
2024-05-11 13:29:37 -0300

    Create DataContext and ConnectionString for OracleDB

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using CineWave.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineWave.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> option) : base(option) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Campanha> Campanhas { get; set;}
+
+        public DbSet<Insights> Insights { get; set; }
+    }
+}
